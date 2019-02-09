8a44493d2a6355eeedf3ff6fc2b9802683da9293
844264935 <844264935@qq.com>
2019-02-09 20:09:28 +0800

    File Structure Changes

--- src/main.c
+++ src/main.c
@@ -27,7 +27,13 @@ GLFWwindow *initWindow(void);
 void processInput(GLFWwindow *window);
 
 float xangle = .0f, yangle = .0f, zangle = .0f;
-float xoffset = .0f, yoffset = 0.f, zoffset = 0.f, scale = 0.5f;
+float xoffset = .0f, yoffset = 0.f, zoffset = 10.f, scale = 0.4f;
+vec3 camera = {0.0f, 0.0f, 3.0f},
+        direction = {0.0f, 0.0f, -1.0f},
+        up = {0.0f, 1.0f, 0.0f};
+float delta_time = 0.0f, last_frame = 0.0f;
+float yaw, pitch;
+float last_x = 200, last_y = 100;
 int mode = MODE_LINE;
 
 int main(int argc, char const *argv[]) {
@@ -50,12 +56,12 @@ int main(int argc, char const *argv[]) {
 int doLoop(GLFWwindow *window) {
     //-------------------Create Shaders------------------------//
     GLuint ShaderProgram = loadProgram(
-            "shaders/VertexShaderSrc.vs",
-            "shaders/FragmentShaderSrc.fs");
+            "shaders/VertexShaderSrc.glsl",
+            "shaders/FragmentShaderSrc.glsl");
     //---------------------------------------------------------//
     //Load Texture
     //Generate Texture
-    GLuint texture1 = loadTexture("textures/container.png", GL_RGBA);
+    GLuint texture1 = loadTexture("textures/face.png", GL_RGBA);
     GLuint texture2 = loadTexture("textures/container.jpg", GL_RGB);
     // Vertext Data & Create Buffers
     float vertices[][8] = {
@@ -173,12 +179,30 @@ int doLoop(GLFWwindow *window) {
             mode = GL_TRIANGLES;
     }
     int flag = 1;
+    struct {
+        float x, y, z;
+    } cube_pos[] = {
+            {0.0f,  0.0f,  0.0f},
+            {2.0f,  5.0f,  -15.0f},
+            {-1.5f, -2.2f, -2.5f},
+            {-3.8f, -2.0f, -12.3f},
+            {2.4f,  -0.4f, -3.5f},
+            {-1.7f, 3.0f,  -7.5f},
+            {1.3f,  -2.0f, -2.5f},
+            {1.5f,  2.0f,  -2.5f},
+            {1.5f,  0.2f,  -1.5f},
+            {-1.3f, 1.0f,  -1.5f},
+    };
     // Loop
     while (!glfwWindowShouldClose(window)) {
+        //delta time process
+        float current_frame = (float) glfwGetTime();
+        delta_time = current_frame - last_frame;
+        last_frame = current_frame;
         // input
         processInput(window);
         // Render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.f);
+        glClearColor(0.8627f, 0.8627f, 0.8627f, 1.f);
         //glClear(GL_COLOR_BUFFER_BIT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // Use Program & Draw
@@ -196,7 +220,10 @@ int doLoop(GLFWwindow *window) {
         mat4x4_rotate_Z(model, model, zangle);
         mat4x4_scale_aniso(model, model, scale, scale, scale);
         //View Matrix
-        mat4x4_translate(view, 0.f, 0.f, -3.0f);
+//        mat4x4_translate(view, 0.f, 0.f, -5.0f);
+        vec3 target;
+        vec3_add(target, camera, direction);
+        mat4x4_look_at(view, camera, target, up);
         //Projection Matrix
         float ratio = 1.0f / 1.0f;
         mat4x4_perspective(projection, 45.f / 180 * PI, ratio, 0.1f, 100.0f);
@@ -208,18 +235,23 @@ int doLoop(GLFWwindow *window) {
         glBindVertexArray(VAO);
         glPolygonMode(GL_FRONT, GL_FILL);
         glEnable(GL_DEPTH_TEST);
-        glDrawElements(GL_TRIANGLES,
-                       sizeof(indices) / sizeof(float),
-                       GL_UNSIGNED_INT, (void *) 0);
+//        glDrawElements(GL_TRIANGLES,
+//                       sizeof(indices) / sizeof(float),
+//                       GL_UNSIGNED_INT, (void *) 0);
 
-        mat4x4_translate(model, 2.5f, 2.5f, -10.f);
-        mat4x4_rotate_X(model, model, xangle);
-        mat4x4_rotate_Y(model, model, yangle);
-        mat4x4_rotate_Z(model, model, zangle);
-        mat4x4_mul(mvp, projection, view);
-        mat4x4_mul(mvp, mvp, model);
-        glUniformMatrix4fv(matrixLoc, 1, GL_FALSE, (const float *) mvp);
-        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(float), GL_UNSIGNED_INT, (void *) 0);
+        for (int i = 0; i < 10; ++i) {
+            mat4x4_translate(model,
+                             cube_pos[i].x, cube_pos[i].y, cube_pos[i].z);
+            float angle = 20.0f * i;
+            mat4x4_rotate(model, model, 1.0f, 0.3f, 0.5f, angle);
+            mat4x4_scale_aniso(model, model, scale, scale, scale);
+            mat4x4_mul(mvp, projection, view);
+            mat4x4_mul(mvp, mvp, model);
+            glUniformMatrix4fv(matrixLoc, 1, GL_FALSE, (const float *) mvp);
+            glDrawElements(GL_TRIANGLES,
+                           sizeof(indices) / sizeof(float),
+                           GL_UNSIGNED_INT, (void *) 0);
+        }
 
         // check events
         glfwPollEvents();
@@ -230,6 +262,20 @@ int doLoop(GLFWwindow *window) {
     return 0;
 }
 
+void walk_along_direction(vec3 camera_pos, vec3 const direction, float const speed) {
+    vec3 temp;
+    vec3_scale(temp, direction, speed);
+    vec3_add(camera_pos, camera_pos, temp);
+}
+
+void walk_sideways(vec3 camera_pos, vec3 const direction, vec3 const up, float const speed) {
+    vec3 temp;
+    vec3_mul_cross(temp, direction, up);
+    vec3_norm(temp, temp);
+    vec3_scale(temp, temp, speed);
+    vec3_add(camera_pos, camera_pos, temp);
+}
+
 /**
  * Process user key inputs
  */
@@ -237,10 +283,11 @@ void processInput(GLFWwindow *window) {
     static double lastTime = 0;
     static float delta1 = 0.001f, delta2 = 1e-3f;
     double t = glfwGetTime();
+    float camera_speed = 2.5f * delta_time;
     const float ratio = 0.025f;
     //
     if (t - lastTime < 0.005) {
-        return;
+        //return;
     }
     lastTime = t;
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -294,6 +341,18 @@ void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
         zoffset -= 0.1f;
     }
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
+        walk_along_direction(camera, direction, camera_speed);
+    }
+    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS) {
+        walk_along_direction(camera, direction, -camera_speed);
+    }
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
+        walk_sideways(camera, direction, up, -camera_speed);
+    }
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS) {
+        walk_sideways(camera, direction, up, camera_speed);
+    }
     scale = scale <= 1e-2f ? 1e-2f : scale;
 }
 
@@ -304,6 +363,40 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 }
 
 void glfw_key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+
+}
+
+void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+    static int first = 1;
+
+    if (first) {
+        last_x = (float) xpos;
+        last_y = (float) ypos;
+        first = 0;
+    }
+
+    float xoffset = (float) xpos - last_x;
+    float yoffset = last_y - (float) ypos;
+    last_x = (float) xpos;
+    last_y = (float) ypos;
+
+    float sensitivity = 0.005f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f) pitch = 89.f;
+    if (pitch < -89.f) pitch = -89.f;
+
+    vec3 front;
+    //pitch = pitch / 180.f * PI;
+    //yaw = yaw / 180.f * PI;
+    front[0] = cosf(pitch) * cosf(yaw);
+    front[1] = sinf(pitch);
+    front[2] = cosf(pitch) * sinf(yaw);
+    vec3_norm(direction, front);
 
 }
 
@@ -326,6 +419,7 @@ GLFWwindow *initWindow(void) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
     // Initialize Glad Function Pointer
@@ -338,6 +432,7 @@ GLFWwindow *initWindow(void) {
     // set resize callback func
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     //set keypress callback
-
+    //set mouse callback
+    glfwSetCursorPosCallback(window, mouse_callback);
     return window;
 }
